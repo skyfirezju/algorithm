b4e8d4d665411e0f3c339f83cdd38d1bc0d5696d
skyfirezju <skyfire_zju@163.com>
2014-02-17 17:00:31 +0800

    Wildcard Matching

--- src/com/algorithm/leetcode/WildcardMatch.java
+++ src/com/algorithm/leetcode/WildcardMatch.java
@@ -48,10 +48,53 @@ public class WildcardMatch {
         }
     }
 	
+	/*
+	 * 思路2：
+	 * 用heads记录s的第一个字符的位置，维持s0,用headp记录p的第一个字符的位置，维持p0
+	 * s0!='\0'
+	 * p0 == '?' || s0 == p0 匹配,heads++,headp++,s和p都后移
+	 * p0 == '*'时，可以匹配一个或多个，star=headp记录星号的位置，p后移，s不动，ss记录s的位置
+	 * star==-1,没有'*'直接return false
+	 * star!=-1,headp=star+1,更新p0,heads=++ss更新s0
+	 * 最后p全部是**组成，return true，否则return false
+	 */
+	public boolean isMatch1(String s, String p) {
+		char s0 = s.length() > 0 ? s.charAt(0):'\0';
+		char p0 = p.length() > 0 ? p.charAt(0):'\0';
+		int heads = 0,headp = 0,star = -1,ss=0;
+		while(s0 != '\0') {
+			if(p0 == '?' || s0 == p0){
+				heads++;headp++;
+				s0 = heads < s.length() ? s.charAt(heads):'\0';
+				p0 = headp < p.length() ? p.charAt(headp):'\0';
+				continue;
+			}
+			if(p0 == '*') {
+				star = headp++;
+				p0 = headp < p.length() ? p.charAt(headp):'\0';
+				ss = heads;
+				continue;
+			}
+			if(star != -1) {
+				headp = star+1;
+				p0 = headp < p.length() ? p.charAt(headp):'\0';
+				heads = ++ss;
+				s0 = heads < s.length() ? s.charAt(heads):'\0';
+				continue;
+			}
+			return false;
+		}
+		while(p0 == '*') {
+			headp++;
+			p0 = headp < p.length() ? p.charAt(headp):'\0';
+		}
+		return p0 == '\0';
+	}
+	
 	public static void main(String[] args) {
 		WildcardMatch wm = new WildcardMatch();
-		String s = "b";
-		String p = "?*?";
-		System.out.println(wm.isMatch(s, p));
+		String s = "abeed";
+		String p = "?b*d";
+		System.out.println(wm.isMatch1(s, p));
 	}
 }
